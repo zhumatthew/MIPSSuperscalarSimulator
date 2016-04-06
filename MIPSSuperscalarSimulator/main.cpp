1ae69ed72d6b209d9f1d894436822c124e606b9a
zhumatthew <zhumatthew@yahoo.com>
2016-04-05 23:04:03 -0600

    Initial Commit

---
+++ MIPSSuperscalarSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIPSSuperscalarSimulator
+//
+//  Created by Matt Zhu on 4/5/16.
+//  Copyright © 2016 ZhuKim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
