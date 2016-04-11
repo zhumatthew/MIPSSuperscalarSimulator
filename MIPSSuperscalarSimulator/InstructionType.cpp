f5adc9bed41ce8a8a2d230792cb7b0f659e55fe3
Doo Hwan Kim <doohwan.kim@janus.com>
2016-04-10 18:29:22 -0600

    InstructionType

---
+++ MIPSSuperscalarSimulator/InstructionType.cpp
@@ -0,0 +1,142 @@
+//
+//  Instruction.cpp
+//  MIPSSuperscalarSimulator
+//
+//  Created by Matt Zhu on 4/7/16.
+//  Copyright © 2016 ZhuKim. All rights reserved.
+//
+
+#include "Instruction.hpp"
+
+InstructionType::InstructionType() {
+    
+}
+
+static int InstructionType::instrTypeDefine(string strOpcode) {
+	if(strOpcode == "ADD" || strOpcode == "SUB" || strOpcode == "MUL"|| strOpcode == "DIV" || strOpcode == "AND")
+		return instrType = 0;
+	else if(strOpcode == "ADDI" || strOpcode == "ANDI" || strOpcode == "ORI" || strOpcode == "XORI" || strOpcode == "SUBI" || strOpcode == "SLLI"|| strOpcode == "SRLI")
+		return instrType = 1;
+	else if(strOpcode == "LW" || strOpcode == "SW"||strOpcode == "BEQ" || strOpcode == "BEQL")
+		return instrType = 2;
+	else if(strOpcode == "J" || strOpcode == "JAL"|| strOpcode == "B")
+		return instrType = 3;
+	else if(strOpcode == "BGEZ"|| strOpcode == "BLEZ")
+		return instrType = 4;
+	else
+		cout << "It is a label for branch instruction";
+	return instrType = 5;
+}
+
+static bool InstructionType::isLabel(string strOpcode) {
+	instrTypeDefine(strOpcode);
+	if (this->instrType == 5)
+		return true;
+	else
+		return false;
+}
+
+static int InstructionType::operatonCodeDefine(string str, int instrType) {
+	int optcode = 0;
+	switch(instrType){
+		case 0:
+			if(str == "ADD") {
+				optcode = 0;
+			} else if(str == "MUL") {
+				optcode =28;
+			} else if(str == "SUB") {
+				optcode =0;
+			} else if(str == "DIV") {
+				optcode =0;
+			}
+		break;
+		case 1:
+			if(str == "ADDI"){
+				optcode = 8;
+			}
+			break;
+		case 2:
+			if(str == "LW") {
+				optcode = 35;
+			} else if(str == "SW") {
+				optcode = 43;
+			} else if(str == "BEQ") {
+				optcode = 4;
+			} else if(str == "BEQL") {
+				optcode = 20;
+			}
+			break;
+		case 3:
+			if(str == "J") {
+				optcode = 2;
+			}
+			break;
+		case 4:
+			if(str == "BGEZ"){
+				optcode = 1;
+			} else if(str == "BLEZ") {
+				optcode = 6;
+			}
+			break;
+		default:
+			optcode = 0;
+	}
+	return optcode;
+}
+
+static int InstructionType::lowSixDigitDefine(string str,int instrType) {
+	int low = 0;
+
+	switch(instrType) {
+		case 0:
+			if(str == "ADD"){
+				low = 32;
+			} else if(str == "MUL") {
+				low = 2;
+			} else if(str == "SUB") {
+				low = 34;
+			} else if(str == "DIV") {
+				low = 26;
+			}
+			break;
+		case 1:
+		case 2:
+		case 3:
+			break;
+		default:
+			low = 0;
+	}
+	return low;
+}
+
+static int InstructionType::middleFiveDigitDefine(string str,int instrType) {
+	int middle = 0;
+
+	switch(instrType) {
+		case 0:
+			if(str == "ADD") {
+				middle = 0;
+			} else if(str == "MUL") {
+				middle = 0;
+			} else if(str == "SUB") {
+				middle = 0;
+			} else if(str == "DIV") {
+				middle =0;
+			}
+			break;
+		case 1:
+		case 2:
+		case 3:
+			break;
+		case 4:
+			if(str == "BGEZ"){
+				middle = 1;
+			} else if(str == "BLEZ") {
+				middle = 0;
+			}
+			break;
+		default:
+			middle = 0;
+	}
+	return middle;
+}
---
+++ MIPSSuperscalarSimulator/InstructionType.hpp
@@ -0,0 +1,31 @@
+//
+//  Instruction.hpp
+//  MIPSSuperscalarSimulator
+//
+//  Created by Matt Zhu on 4/7/16.
+//  Copyright © 2016 ZhuKim. All rights reserved.
+//
+
+#ifndef InstructionType_hpp
+#define InstructionType_hpp
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class InstructionType {
+    
+public:
+	InstructionType();
+	static int instrTypeDefine(string strOpcode);
+	static bool isLabel(string strOpcode);
+	static int operatonCodeDefine(string str, int instrType);
+	static int lowSixDigitDefine(string str,int instrType);
+	static int middleFiveDigitDefine(string str,int instrType);
+
+private:
+	static int instrType;
+};
+
+#endif /* InstructionType_hpp */
