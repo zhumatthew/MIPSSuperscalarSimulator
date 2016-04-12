883eb6a338610a18710491c2931d63cde7612e33
zhumatthew <zhumatthew@yahoo.com>
2016-04-12 00:21:05 -0600

    fixed error

--- MIPSSuperscalarSimulator/SimulationInstruction.cpp
+++ MIPSSuperscalarSimulator/SimulationInstruction.cpp
@@ -13,4 +13,4 @@ using namespace std;
 
 SimulationInstruction::SimulationInstruction(string str) : originalString(str), opcode(0), rd(0), rs(0), rt(0), rdValue(0), rtValue(0), rsValue(0), immediate(0), opcodeString(str), currentForward(Forwarding()), effectiveAddress(0), branchCondition(false), reordered(false), instructionLocation(0), loopCount(0) {}
 
-SimulationInstruction::SimulationInstruction(Instruction instr) : opcodeString(instr.opcode), rd(instr.rd), rs(instr.rs), rt(instr.rt), rdValue(instr.rdvalue), rsValue(instr.rsvalue), immediate(instr.immediate), originalString(instr.originalString), opcodeString(instr.opcodeString), currentForward(Forwarding()), effectiveAddress(0), branchCondition(false), reordered(false) {}
+SimulationInstruction::SimulationInstruction(Instruction instr) : rd(instr.rd), rs(instr.rs), rt(instr.rt), rdValue(instr.rdvalue), rsValue(instr.rsvalue), immediate(instr.immediate), originalString(instr.originalString), opcodeString(instr.opcodeString), currentForward(Forwarding()), effectiveAddress(0), branchCondition(false), reordered(false) {}
